654c24b47953695f3e9159046b7f66890ffa5c1e
andresdiazalvarez <ingenierotecni3@gmail.com>
2026-07-29 22:59:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="marcafacil-github-v2",A=["./","./index.html","./app.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./vendor/pdf.min.mjs","./vendor/pdf.worker.min.mjs","./vendor/jspdf.umd.min.js"];
+const C="marcafacil-github-v3",A=["./","./index.html","./app.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./vendor/pdf.min.mjs","./vendor/pdf.worker.min.mjs","./vendor/jspdf.umd.min.js"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
   self.skipWaiting();
